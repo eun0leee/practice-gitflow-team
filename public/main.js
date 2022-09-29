d68f1c662a4fc91006304e2278c0663b5d946106
0nesan <0nesan.o8o@gmail.com>
2022-09-29 15:12:12 +0900

    feat: Create button toggle function. toggle style

---
+++ public/main.js
@@ -0,0 +1,23 @@
+const numberInput = document.querySelector("input");1
+const box = document.querySelector(".box");
+
+const handleToggle = () => {
+    if (box.classList.contains("toggle")){
+        box.classList.remove("toggle");
+    } else {
+        box.classList.add("toggle");
+    } 
+}
+
+
+const handleInput = () => {
+    let val = numberInput.value;
+
+    if(!box.classList.contains("toggle")){
+        box.classList.add("toggle"); 
+    }
+
+    box.innerText(`${val} 명`);
+}
+
+
